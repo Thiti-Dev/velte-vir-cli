3ce2011d7c0f278daaf0dcfb6993313d1bafb92c
Thiti-Dev <thiti.mwk.main@gmail.com>
2022-07-16 17:09:16 +0700

    added (core) virtual-cmd/constants

---
+++ src/core/virtual-cmd/constants/index.ts
@@ -0,0 +1,4 @@
+export const CLEAR_CMD: string = "clear";
+export const UN_RECOGNIZE_CMD: string = "un_recognize_cmd";
+export const WHO_AM_I_CMD: string = "whoami";
+export const CD_CMD: string = "cd";
